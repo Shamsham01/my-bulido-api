a3b4b2ce228907dd0e62eee7ef4bf5dc2ce58629
HodlTokenClub <98131892+Shamsham01@users.noreply.github.com>
2025-02-08 22:45:03 +0000

    Update fungible.ts

--- src/routes/fungible.ts
+++ src/routes/fungible.ts
@@ -1,1 +1,11 @@
+// src/routes/fungible.ts
+import { Router } from 'express';
+import { issueFungibleToken } from '../controllers/ftController';
 
+const router = Router();
+
+// Define your endpoint
+router.post('/issue', issueFungibleToken);
+
+// Export the router so it can be imported in other files.
+export default router;
