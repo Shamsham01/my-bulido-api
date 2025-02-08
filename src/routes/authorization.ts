66a5ca1bd56b47fadf11c9b6b49feaafe2a8ad1b
HodlTokenClub <98131892+Shamsham01@users.noreply.github.com>
2025-02-08 22:31:29 +0000

    Update authorization.ts

--- src/routes/authorization.ts
+++ src/routes/authorization.ts
@@ -1,1 +1,10 @@
+// src/routes/authorization.ts
+import { Router } from 'express';
+import { authorizeConnection } from '../controllers/authController';
 
+const router = Router();
+
+// POST /authorization
+router.post('/', authorizeConnection);
+
+export default router;
