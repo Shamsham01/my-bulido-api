f998dfb789e739b2937b7b1b27f30f4f02b12bc1
HodlTokenClub <98131892+Shamsham01@users.noreply.github.com>
2025-02-08 23:27:23 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // src/index.ts
 import express from 'express';
 import bodyParser from 'body-parser';
-import authorizationRoutes from './routes/authorization';
-import fungibleRoutes from './routes/fungible'; // Make sure the file exists at this location
+import authorizationRoutes from './routes/authorization.js';
+import fungibleRoutes from './routes/fungible.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
